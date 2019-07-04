62d1f00627c412c3a168dfb2cd28961eb55c95fa
Chris Haegner <chris@stoyo.io>
2019-07-04 18:01:47 +0200

    no frontend but backend works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+json = open(url).read
+ingredients = JSON.parse(json)
+
+ingredients['drinks'].each do |hash|
+  Ingredient.create(name: hash['strIngredient1'])
+end
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
